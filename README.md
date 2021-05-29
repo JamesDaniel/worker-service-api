c5e1523b3cdb104e3b1a37be21df4ed724104111
JamesDaniel <jamesmcgarr@protonmail.com>
2021-05-29 23:15:41 +0100

    Added JWT with keycloak

---
+++ README.md
@@ -0,0 +1,18 @@
+# Job API
+A POC application to submit and retrieve images
+
+## References
+
+ - Docker
+   - https://docs.docker.com/develop/develop-images/multistage-build/
+   - https://docs.docker.com/get-started/02_our_app/
+   - https://hub.docker.com/r/adoptopenjdk/maven-openjdk11/tags?page=1&ordering=last_updated
+   - https://spring.io/guides/gs/spring-boot-docker/
+   - https://chtc.cs.wisc.edu/docker-test.shtml
+   - https://github.com/JamesDaniel/spring-boot-docker/blob/master/Dockerfile
+ - Spring
+   - https://www.baeldung.com/spring-resttemplate-post-json
+   - https://auth0.com/blog/spring-boot-authorization-tutorial-secure-an-api-java/
+   - https://app.pluralsight.com/library/courses/securing-spring-data-rest-apis/table-of-contents
+   - https://github.com/jzheaux/resolutions
+   
--- src/main/java/com/jamesmcgarr/worker/controller/JobController.java
+++ src/main/java/com/jamesmcgarr/worker/controller/JobController.java
@@ -1,5 +1,7 @@
 package com.jamesmcgarr.worker.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -8,8 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 public class JobController {
 
     @GetMapping(value = "api/v1/job/{id}/status", produces = "application/json", consumes = "application/json")
-    public String test(@PathVariable int id) {
-        return "test response " + id;
+    public String test(@PathVariable int id, @AuthenticationPrincipal Jwt jwt) {
+        System.out.println(jwt.getClaims());
+        System.out.println("-- " + jwt.getClaim("tid"));
+        System.out.println("-- " + jwt.getClaim("oid"));
+        System.out.println("-- " + jwt.getClaim("aud"));
+        System.out.println("-- " + jwt.getClaim("azp"));
+        System.out.println("-- " + jwt.getClaim("name"));
+        System.out.println("-- " + jwt.getClaim("email"));
+        return "test response " + id; // `RUNNING`,`SUCCESS`,`FAILED`
     }
 
     @PostMapping(value = "api/v1/job", produces = "application/json", consumes = "application/json")
